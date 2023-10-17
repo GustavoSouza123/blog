a8b91d5893b76ade18a23ee5127ed1ca46f2607d
Gustavo Souza <gustavoelia7@gmail.com>
2023-10-17 20:39:36 -0300

    initial commit: structure and initial files

---
+++ main.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Painel de Controle | Code Universe</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Painel de Controle</h1>    
+
+    <script src="js/script.js"></script>
+</body>
+</html>
