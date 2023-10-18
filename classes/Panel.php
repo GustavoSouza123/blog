00b0a8656561a163247c9637cf0f820088281d5d
Gustavo Souza <gustavoelia7@gmail.com>
2023-10-17 21:14:10 -0300

    feats: autoload classes, login verification

---
+++ classes/Panel.php
@@ -0,0 +1,12 @@
+<?php
+    class Panel {
+        public static function isLogged() {
+            return (isset($_SESSION['codeuniverse-login'])) ? true : false;
+        }
+
+        public static function logout() {
+            session_destroy();
+            header('Location: '.INCLUDE_PATH);
+        }
+    }
+?>
--- config/config.php
+++ config/config.php
@@ -1,3 +1,12 @@
 <?php
+    session_start();
+
+    /* autoload classes */
+    $autoload = function($class) {
+        require_once 'classes/'.$class.'.php';
+    };
+    spl_autoload_register($autoload);
+
+    /* constants variables */
     define('INCLUDE_PATH', 'http://localhost/code-universe');
 ?>
