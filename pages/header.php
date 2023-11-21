6e118b2012aa4b0637824b91546e1e6a3ebf6ca4
Gustavo Souza <gustavoelia7@gmail.com>
2023-11-21 14:53:14 -0300

    fixes

--- pages/header.php
+++ pages/header.php
@@ -1,25 +1,3 @@
-<!-- <header>
-    <div class="content">
-        <nav class="register">
-        <?php //if(Panel::isLogged()) { ?>
-            <ul class="profile">
-                <a href="<?php //echo INCLUDE_PATH_ADMIN; ?>">
-                    <div class="profile-photo">
-                        <img src="<?php //echo INCLUDE_PATH_ADMIN.$_SESSION['myblog-profile-photo']; ?>" alt="Foto de perfil" />
-                    </div>
-                    <div class="profile-name"><?php //echo $_SESSION['myblog-name']; ?></div>
-                </a>
-            </ul>
-        <?php //} else { ?>
-            <ul class="register">
-                <li><a href="<?php //echo INCLUDE_PATH_ADMIN; ?>login">Entrar</a></li>
-                <li><a href="<?php //echo INCLUDE_PATH_ADMIN; ?>signup">Cadastrar</a></li>
-            </ul>
-        <?php //} ?>
-        </nav>
-    </div>
-</header> -->
-
 <header>
     <div class="social">
         <a href="https://github.com/GustavoSouza123" target="_blank"><img src="<?php echo INCLUDE_PATH_PORTFOLIO; ?>assets/images/github.svg" alt="Github logo" /></a>
@@ -31,7 +9,7 @@
             <li><a href="<?php echo INCLUDE_PATH_PORTFOLIO; ?>">Home</a></li>
             <li><a href="">About</a></li>
             <li><a href="">Projects</a></li>
-            <li><a class="active" href="">Blog</a></li>
+<li><a class="active" href="<?php echo INCLUDE_PATH; ?>">Blog</a></li>
             <li><a href="">Contact</a></li>
         </ul>
     </nav> 
