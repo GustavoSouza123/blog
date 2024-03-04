6ea49fe4355eec5c14ce332cfa58c65433aa9cf3
Gustavo Souza <gustavoelia7@gmail.com>
2024-03-04 11:54:36 -0300

    feat: browse categories (finished)

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,15 @@
 <?php include 'header.php'; ?>
 
+<?php
+    $currentCategory = isset($_GET['category']) ? $_GET['category'] : '%%';
+?>
+
 <main class="blog">
     <div class="content">
         <section class="posts">
             <h1 class="title"><?= $content->blogTitle ?></h1>
             <div class="categories">
+                <div class="category"><a href="<?= INCLUDE_PATH ?>">Todas</a></div>
                 <?php
                     $sql = $pdo->prepare("SELECT * FROM `tb_categories`");
                     $sql->execute();
@@ -18,26 +23,30 @@
             </div>
             <div class="posts-content">
             <?php
-                $sql = $pdo->prepare("SELECT * FROM `tb_posts` WHERE published = 1");
-                $sql->execute();
-                $posts = $sql->fetchAll(PDO::FETCH_ASSOC);
-                foreach($posts as $key => $value) {
-                    $sql = $pdo->prepare("SELECT `name` FROM `tb_categories` WHERE id = ?");
-                    $sql->execute(array($value['category_id']));
-                    $category = $sql->fetchColumn();
-                    echo '
-                    <div class="post">
-                        <div class="image">
-                            <img src="'.INCLUDE_PATH_ADMIN.$value['thumbnail'].'" alt="" />
+                $sql = $pdo->prepare("SELECT p.*, c.name FROM `tb_posts` AS `p` INNER JOIN `tb_categories` AS `c` ON p.category_id = c.id WHERE c.name LIKE ? AND p.published = 1");
+                $sql->execute(array($currentCategory));
+                if($sql->rowCount() == 0) {
+                    echo 'Nenhum post encontrado';
+                } else {
+                    $posts = $sql->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($posts as $key => $value) {
+                        $sql = $pdo->prepare("SELECT `name` FROM `tb_categories` WHERE id = ?");
+                        $sql->execute(array($value['category_id']));
+                        $category = $sql->fetchColumn();
+                        echo '
+                        <div class="post">
+                            <div class="image">
+                                <img src="'.INCLUDE_PATH_ADMIN.$value['thumbnail'].'" alt="" />
+                            </div>
+                            <div class="content">
+                                <div class="title">'.$value['title'].'</div>
+                                <div class="subtitle">'.$value['subtitle'].'</div>
+                                <div class="read-more"><a href="article?id='.$value['id'].'">Ler mais</a></div>
+                                <div class="category"><a href="'.INCLUDE_PATH.'?category='.$category.'">'.$category.'</a></div>
+                            </div>
                         </div>
-                        <div class="content">
-                            <div class="title">'.$value['title'].'</div>
-                            <div class="subtitle">'.$value['subtitle'].'</div>
-                            <div class="read-more"><a href="article?id='.$value['id'].'">Ler mais</a></div>
-                            <div class="category"><a href="'.INCLUDE_PATH.'?category='.$category.'">'.$category.'</a></div>
-                        </div>
-                    </div>
-                    ';
+                        ';
+                    }
                 }
             ?>
             </div>
