46fad3b5fd9a15419dd1cc4313273535995bbfb3
Gustavo Souza <gustavoelia7@gmail.com>
2024-04-01 19:59:24 -0300

    feat: google ads (script and meta tags and ads.txt)

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="My tech blog | Gustavo Souza"> <!-- seo -->
     <meta name="keywords" content="my,tech,blog,web,programming,coding"> <!-- seo -->
+    <meta name="google-adsense-account" content="ca-pub-7508322719547433"> <!-- google ads -->
     <link rel="icon" type="image/x-icon" href=""> <!-- website icon -->
     <!-- css -->
     <style>
@@ -31,11 +32,11 @@
             visibility: hidden;
             background: <?php if($theme == 'dark') echo '#222'; else echo '#fff'; ?>;
         }
-    </style>
+        </style>
     <?php
         $url = (isset($_GET['url'])) ? $_GET['url'] : 'home';
         $cssName = ($url == 'article') ? 'article' : 'blog';
-    ?>
+        ?>
     <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- google fonts api -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- google fonts api -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"> <!-- Open Sans from google fonts api -->
@@ -47,6 +48,7 @@
     <!-- javascript -->
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script> <!-- jQuery API -->
     <script src="https://kit.fontawesome.com/52201d9086.js" crossorigin="anonymous"></script> <!-- font awesome icons -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7508322719547433" crossorigin="anonymous"></script> <!-- google ads -->
     <script>
         // page loading
         /*$(window).on('load', function() {
